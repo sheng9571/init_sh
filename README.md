5c664e21a4f7f26f5aebe2cb4ffc1a8fc70456ee
sheng <sheng9571@gmail.com>
2018-04-13 15:19:14 +0800

    Init project ...

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # init_sh
 This is a project that about some initial settings based on ubuntu
+```
+Install
+git clone --recursive https://github.com/sheng9571/init_sh
+```
---
+++ init.sh
@@ -0,0 +1,26 @@
+# change source list
+sudo sed -i '1, $s/tw.archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
+
+
+# install requirement packages
+sudo apt-get update
+sudo apt-get install -y ntp ntpdate python-setuptools python-dev build-essential vim tmux zsh git openssh-server libxml2-dev libxslt-dev
+
+
+# time correction
+export TZ='Asia/Taipei';
+sudo /etc/init.d/ntp stop && sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && sudo ntpdate tw.pool.ntp.org && sudo /etc/init.d/ntp start
+
+
+# install easy_install、pip、requests、lxml
+sudo easy_install pip
+sudo pip install --upgrade pip requests lxml
+
+
+# set default shell and link config files
+chsh /usr/bin/zsh
+cd ~
+git clone --recursive https://github.com/sheng9571/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+make
+cd ~
